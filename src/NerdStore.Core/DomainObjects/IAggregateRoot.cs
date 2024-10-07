643adf5dd3b511af95273fdac0b4080b7032fe62
santtose <santtose@outlook.com>
2024-10-07 14:48:00 -0300

    criando primeiras entidades

---
+++ src/NerdStore.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,5 @@
+﻿namespace NerdStore.Core.DomainObjects
+{
+    // Interface de marcação. Serve basicamente para dizer que a classe que a está implementando é uma classe raiz de agregação.
+    public interface IAggregateRoot { }
+}
