9fe13caa56eca014302d4a2dc7a236a31bdff772
santtose <santtose@outlook.com>
2024-10-14 15:59:03 -0300

    implementando objetos de dominio

--- src/NerdStore.Vendas.Domain/Voucher.cs
+++ src/NerdStore.Vendas.Domain/Voucher.cs
@@ -1,8 +1,27 @@
 ﻿using NerdStore.Core.DomainObjects;
+using System.ComponentModel.DataAnnotations;
 
 namespace NerdStore.Vendas.Domain
 {
     public class Voucher : Entity
     {
+        public string Codigo { get; private set; }
+        public decimal? Percentual { get; private set; }
+        public decimal? ValorDesconto { get; private set; }
+        public int Quantidade { get; private set; }
+        public TipoDescontoVoucher TipoDescontoVoucher { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+        public DateTime? DataUtilizacao { get; private set; }
+        public DateTime DataValidade { get; private set; }
+        public bool Ativo { get; private set; }
+        public bool Utilizado { get; private set; }
+
+        // EF Rel.
+        public ICollection<Pedido> Pedidos { get; set; }
+
+        //internal ValidationResult ValidarSeAplicavel()
+        //{
+        //    return new VoucherAplicavelValidation().Validate(this);
+        //}
     }
 }
