6082b96f730ea760dc9760182d5e80ba936153f7
santtose <santtose@outlook.com>
2024-10-24 09:12:39 -0300

    Add categoriaTests no commit

---
+++ tests/NerdStore.catalogo.Domain.Tests/CategoriaTests.cs
@@ -0,0 +1,24 @@
+﻿using NerdStore.Core.DomainObjects;
+using NerdStote.Catalogo.Domain;
+
+namespace NerdStore.catalogo.Domain.Tests
+{
+    public class CategoriaTests
+    {
+        [Fact]
+        public void Categoria_Validar_ValidacoesDevemRetornarExceptions()
+        {
+            var ex = Assert.Throws<DomainException>(() =>
+                new Categoria(string.Empty, 10)
+            );
+
+            Assert.Equal("O campo Nome da categoria não pode estar vazio", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() =>
+                new Categoria("Nome Qualquer", 0)
+            );
+
+            Assert.Equal("O campo Codigo não pode ser 0", ex.Message);
+        }
+    }
+}
